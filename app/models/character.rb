46ca2849d0ef0c6ada9129fb88d1510122c974fe
Blake Julander <13lakejulander@gmail.com>
2021-02-17 15:34:35 -0700

    commit tree c84cac34ecfc7773bcf32f22461aa84ea59e9636

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor #actors
+  belongs_to :show #shows
+
+  def say_that_thing_you_say
+    # self.catchphrase
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
